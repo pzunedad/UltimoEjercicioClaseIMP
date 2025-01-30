e4a2a03af42e23f51fb571db237f2cefb7568aa7
pzunedad <pedro.zunedadiego@gmail.com>
2025-01-30 12:45:12 +0100

    change main.ts

--- main.ts
+++ main.ts
@@ -3,19 +3,22 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs } from "./schema.ts";
 import { resolvers } from "./resolvers.ts";
-import { GraphQLError } from "graphql";
 import { ContactModel } from "./types.ts";
 
+// Obtener la URL de MongoDB desde las variables de entorno
 const MONGO_URL = Deno.env.get("MONGO_URL");
-if (!MONGO_URL) throw new GraphQLError("MONGO URL NOT EXISTS");
+
+if (!MONGO_URL) {
+    console.error("❌ Error: La variable de entorno MONGO_URL no está definida.");
+    Deno.exit(1);
+}
 
 const client = new MongoClient(MONGO_URL);
 await client.connect();
-console.log("Conectado a la base de datos");
+console.log("✅ Conectado a la base de datos");
 
 const db = client.db("agenda");
 const ContactCollection = db.collection<ContactModel>("contact");
-
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
@@ -23,4 +26,4 @@ const { url } = await startStandaloneServer(server, {
     context: async () => ({ ContactCollection }),
 });
 
-console.log(`🚀  Server ready at: ${url}`);
+console.log(`🚀 Server ready at: ${url}`);
